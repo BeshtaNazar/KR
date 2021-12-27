2b5949c1783af70b2c6acb780f14472982a17c34
BeshtaNazar <76489562+BeshtaNazar@users.noreply.github.com>
2021-12-27 22:50:16 +0200

    Add files via upload

---
+++ Курсова/Курсова/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Курсова
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.p0p0 = new System.Windows.Forms.PictureBox();
+            this.p0p1 = new System.Windows.Forms.PictureBox();
+            this.p0p2 = new System.Windows.Forms.PictureBox();
+            this.p1p0 = new System.Windows.Forms.PictureBox();
+            this.p1p1 = new System.Windows.Forms.PictureBox();
+            this.p1p2 = new System.Windows.Forms.PictureBox();
+            this.p2p0 = new System.Windows.Forms.PictureBox();
+            this.p2p1 = new System.Windows.Forms.PictureBox();
+            this.p2p2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.p0p0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p0p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p0p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1p0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2p0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2p2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p0p0
+            // 
+            this.p0p0.BackColor = System.Drawing.SystemColors.Control;
+            this.p0p0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p0p0.Location = new System.Drawing.Point(604, 38);
+            this.p0p0.Name = "p0p0";
+            this.p0p0.Size = new System.Drawing.Size(133, 103);
+            this.p0p0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p0p0.TabIndex = 0;
+            this.p0p0.TabStop = false;
+            this.p0p0.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p0p1
+            // 
+            this.p0p1.BackColor = System.Drawing.SystemColors.Control;
+            this.p0p1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p0p1.Location = new System.Drawing.Point(732, 38);
+            this.p0p1.Name = "p0p1";
+            this.p0p1.Size = new System.Drawing.Size(131, 103);
+            this.p0p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p0p1.TabIndex = 1;
+            this.p0p1.TabStop = false;
+            this.p0p1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p0p2
+            // 
+            this.p0p2.BackColor = System.Drawing.SystemColors.Control;
+            this.p0p2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p0p2.Location = new System.Drawing.Point(859, 38);
+            this.p0p2.Name = "p0p2";
+            this.p0p2.Size = new System.Drawing.Size(133, 103);
+            this.p0p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p0p2.TabIndex = 2;
+            this.p0p2.TabStop = false;
+            this.p0p2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p1p0
+            // 
+            this.p1p0.BackColor = System.Drawing.SystemColors.Control;
+            this.p1p0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p1p0.Location = new System.Drawing.Point(604, 136);
+            this.p1p0.Name = "p1p0";
+            this.p1p0.Size = new System.Drawing.Size(133, 103);
+            this.p1p0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1p0.TabIndex = 3;
+            this.p1p0.TabStop = false;
+            this.p1p0.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p1p1
+            // 
+            this.p1p1.BackColor = System.Drawing.SystemColors.Control;
+            this.p1p1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p1p1.Location = new System.Drawing.Point(732, 136);
+            this.p1p1.Name = "p1p1";
+            this.p1p1.Size = new System.Drawing.Size(133, 103);
+            this.p1p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1p1.TabIndex = 4;
+            this.p1p1.TabStop = false;
+            this.p1p1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p1p2
+            // 
+            this.p1p2.BackColor = System.Drawing.SystemColors.Control;
+            this.p1p2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p1p2.Location = new System.Drawing.Point(859, 136);
+            this.p1p2.Name = "p1p2";
+            this.p1p2.Size = new System.Drawing.Size(133, 103);
+            this.p1p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1p2.TabIndex = 5;
+            this.p1p2.TabStop = false;
+            this.p1p2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p2p0
+            // 
+            this.p2p0.BackColor = System.Drawing.SystemColors.Control;
+            this.p2p0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p2p0.Location = new System.Drawing.Point(604, 234);
+            this.p2p0.Name = "p2p0";
+            this.p2p0.Size = new System.Drawing.Size(133, 103);
+            this.p2p0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2p0.TabIndex = 6;
+            this.p2p0.TabStop = false;
+            this.p2p0.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p2p1
+            // 
+            this.p2p1.BackColor = System.Drawing.SystemColors.Control;
+            this.p2p1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p2p1.Location = new System.Drawing.Point(732, 234);
+            this.p2p1.Name = "p2p1";
+            this.p2p1.Size = new System.Drawing.Size(131, 103);
+            this.p2p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2p1.TabIndex = 7;
+            this.p2p1.TabStop = false;
+            this.p2p1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // p2p2
+            // 
+            this.p2p2.BackColor = System.Drawing.SystemColors.Control;
+            this.p2p2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p2p2.Location = new System.Drawing.Point(859, 234);
+            this.p2p2.Name = "p2p2";
+            this.p2p2.Size = new System.Drawing.Size(133, 103);
+            this.p2p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2p2.TabIndex = 8;
+            this.p2p2.TabStop = false;
+            this.p2p2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 52);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Перемішати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 48);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Завантижити картинку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1480, 646);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.p2p2);
+            this.Controls.Add(this.p2p1);
+            this.Controls.Add(this.p2p0);
+            this.Controls.Add(this.p1p2);
+            this.Controls.Add(this.p1p1);
+            this.Controls.Add(this.p1p0);
+            this.Controls.Add(this.p0p2);
+            this.Controls.Add(this.p0p1);
+            this.Controls.Add(this.p0p0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.pictureBox1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.p0p0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p0p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p0p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1p0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2p0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2p2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p0p0;
+        private System.Windows.Forms.PictureBox p0p1;
+        private System.Windows.Forms.PictureBox p0p2;
+        private System.Windows.Forms.PictureBox p1p0;
+        private System.Windows.Forms.PictureBox p1p1;
+        private System.Windows.Forms.PictureBox p1p2;
+        private System.Windows.Forms.PictureBox p2p0;
+        private System.Windows.Forms.PictureBox p2p1;
+        private System.Windows.Forms.PictureBox p2p2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Курсова/Курсова/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Курсова
+{
+    public partial class Form1 : Form
+    {
+        string s = null;
+        Bitmap[,] bmps;
+        PictureBox[,] arr;
+        public Form1()
+        {
+            InitializeComponent();
+            Draw();
+        }
+        private void Draw(string path = "123.jpg")
+        {
+            Image img = Image.FromFile(path);
+            int widthThird = (int)((double)img.Width / 3.0);
+            int heightThird = (int)((double)img.Height / 3.0);
+            bmps = new Bitmap[3, 3];            
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    bmps[i, j] = new Bitmap(widthThird, heightThird);
+                    Graphics g = Graphics.FromImage(bmps[i, j]);
+                    g.DrawImage(img, new Rectangle(0, 0, widthThird, heightThird), new Rectangle(j * widthThird,
+                        i * heightThird, widthThird, heightThird), GraphicsUnit.Pixel);
+                    g.Dispose();
+                }                
+            p0p0.Image = bmps[0, 0];
+            p0p1.Image = bmps[0, 1];
+            p0p2.Image = bmps[0, 2];
+            p1p0.Image = bmps[1, 0];
+            p1p1.Image = bmps[1, 1];
+            p1p2.Image = bmps[1, 2];
+            p2p0.Image = bmps[2, 0];
+            p2p1.Image = bmps[2, 1];
+            p2p2.Image = bmps[2, 2];
+        }
+        private void Random(PictureBox[,] arr)
+        {
+            Random r = new Random();
+            int x = 0, y = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    x = r.Next(0, 2);
+                    y = r.Next(0, 2);
+                    Image img = arr[i, j].Image;
+                    arr[i, j].Image = arr[x, y].Image;
+                    arr[x, y].Image = img;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            arr = new PictureBox[,] { { p0p0, p0p1, p0p2 }, { p1p0, p1p1, p1p2 }, { p2p0, p2p1, p2p2 } };
+            Random(arr);
+            timer1.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+            if (s == null)
+            {
+                s = pb.Name;
+                pb.BorderStyle = BorderStyle.Fixed3D;
+            }
+            else
+            {
+                Swap(s, pb.Name);
+                PictureBox pic2 = (PictureBox)this.Controls.Find(s, false).First();
+                pic2.BorderStyle = BorderStyle.FixedSingle;
+                s = null;
+            }
+        }
+        private void Swap(string from, string to)
+        {
+            PictureBox pb1 = (PictureBox)this.Controls.Find(from, false).First();
+            PictureBox pb2 = (PictureBox)this.Controls.Find(to, false).First();
+            Image img = pb1.Image;
+            pb1.Image = pb2.Image;
+            pb2.Image = img;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (bmps[i, j] == arr[i, j].Image)
+                    {
+                        count++;
+                    }
+                }
+            }
+            if (count == 9)
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+                timer1.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random(arr);
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Draw(openFileDialog.FileName); Random(arr);
+                }
+            }
+        }
+    }
+}
